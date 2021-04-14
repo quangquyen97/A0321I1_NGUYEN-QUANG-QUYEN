8e31f370af15837d22785467766a966f015d108d
quangquyen97 <77972443+quangquyen97@users.noreply.github.com>
2021-04-14 19:40:18 +0700

    Bien,dulieu_va_toan_tu

---
+++ MODULE1/7.Bien_va_kieu_du_lieu_toan_tu/Bai_Tap/chuyen_doi_tien_te.js
@@ -0,0 +1,10 @@
+var inputA;
+var usdvnd= 23000;
+var wonvnd=13000;
+var usdwon=10000;
+
+inputA=prompt(document.getElementById(Nhap));
+vnd=parseInt(inputA);
+let a= vnd * usdvnd;
+let b= vnd * wonvnd;
+let c=
